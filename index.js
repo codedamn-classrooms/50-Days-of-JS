851ba2ab2f87ba1a50c77121d2a79579b695b814
Vikrant Bhat <vik.bhat223@gmail.com>
2021-11-10 16:20:45 +0530

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+
+function getDaysBetweenDates(dateText1, dateText2) {
+    // write your solution here
+
+    return
+}
+
+module.exports = { getDaysBetweenDates }
