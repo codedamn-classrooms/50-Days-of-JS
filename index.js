cc5e7c72c0ea3c694042ba2c18e227cd46803317
Vikrant Bhat <vik.bhat223@gmail.com>
2021-11-10 16:06:43 +0530

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const str = 'JavaScript is simple but not easy to master';
+
+function truncateWithWordLimit(str) {
+    // write your solution here
+
+    return
+}
+
+module.exports = { truncateWithWordLimit }
