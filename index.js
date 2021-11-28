24bdfeaa4f3c53615ab2e842eb421b0c258621f9
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2021-11-28 17:33:11 +0530

    challenge 18

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const isPangram = (input) => {
+const totalGrains = (input) => {
 	// Code here
 
 	return
 }
+
+const grainsOn = (input) => {
+	// Code here
+
+	return
+}
+
+console.log(`Grains on 5th square: ${grainsOn(5)}`)
+console.log(`Total grains upto 5th square: ${totalGrains(5)}`)
