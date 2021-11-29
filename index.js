928d73bd14fc8e49d1492194c5586521afb75404
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2021-11-29 21:36:08 +0530

    challenge 19

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const totalGrains = () => {
+const colorCode = (color) => {
 	// Code here
 
 	return
 }
-
-const grainsOn = (input) => {
-	// Code here
-
-	return
-}
-
-console.log(`Grains on 5th square: ${grainsOn(5)}`)
-console.log(`Total grains on the Chess Board: ${totalGrains()}`)
