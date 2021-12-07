4b2de0b7eb3d0f53fe44c6c96db706bf2e7a6594
Pranav <pranav.techiegeek@gmail.com>
2021-12-07 15:21:17 +0530

    ready

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // you JavaScript magic goes here
+
+// Onclick on #set-timer-btn the timer should be set to the input values
+
+// input should be taken from #hours-input & #minutes-input & #seconds-input
+
+// the values should be set at #hours-counter & #minutes-counter & #seconds-counter
+
+// Onclick on #start-timer-btn the timer should start counting
+// Onclick on #stop-timer-btn the timer should stop counting
