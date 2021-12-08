49a270daeae25b388ca95806a56e9e9f38e502c7
Pranav <pranav.techiegeek@gmail.com>
2021-12-08 17:05:43 +0530

    boilerplate for js file

--- index.js
+++ index.js
@@ -8,3 +8,26 @@
 
 // Onclick on #start-timer-btn the timer should start counting
 // Onclick on #stop-timer-btn the timer should stop counting
+
+const hoursCounter = document.getElementById("hours-counter");
+const minutesCounter = document.getElementById("minutes-counter");
+const secondsCounter = document.getElementById("seconds-counter");
+
+const hoursInput = document.getElementById("hours-input");
+const minutesInput = document.getElementById("minutes-input");
+const secondsInput = document.getElementById("seconds-input");
+
+function setCountdownTimer() {
+  // write the code here to set the timer values
+}
+
+function startTimer() {
+  //code to start the Timer
+}
+function stopTimer() {
+  // code to stop the timer
+}
+
+function passTests() {
+  return "pass";
+}
