fc866bc8069f0425219f1f3ce6a2e396318f79a5
Pranav <pranav.techiegeek@gmail.com>
2021-12-08 12:02:01 +0530

    ready

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-function spinWords(string) {
-  //TODO Have fun :)
+function toWeirdCase(string) {
+  // Your code goes here
 }
+
+console.log(
+  `The weird case of ${"A test case"} is ${toWeirdCase("A test case")}`
+);
