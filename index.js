04894dfa92f9c43c09b64744bfc8ae459baddc55
Vikrant Bhat <vik.bhat223@gmail.com>
2021-12-11 13:12:19 +0530

    init

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const sayNumberInEnglish = (n /* ADD MORE PARAMETERS IF NEEDED */) => {
-	// Write your solution here
-	return
-}
-
-console.log(`5635 in english is: ${sayNumberInEnglish(5635)}`)
+// Write your code here
