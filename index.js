4a27163aeabd2cde1de5e628c680d155d9684f07
Vikrant Bhat <vik.bhat223@gmail.com>
2021-12-12 10:20:58 +0530

    init

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function factorial(n) {
+function gcd(a, b) {
 	// write your code here
 	return
 }
 
-let n = 4;
-console.log("The factorial of " + n + " is " + factorial(n));
+const a = 2154
+const b = 458
+
+console.log("The GCD of " + a + " ", b + " is " + gcd(a, b));
